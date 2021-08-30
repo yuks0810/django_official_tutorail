043ec472dde5eb9c035d35ca85a8268d9ad0bc5e
PC732_nakata_yuki <>
2021-08-31 00:11:30 +0900

    reactサイドからdjangoのAPIの値を取得して、画面に描画するまで完成

--- frontend/my-app/src/App.js
+++ frontend/my-app/src/App.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Nav from './components/react_router_tutorial/Nav';
-import About from './components/react_router_tutorial/About';
-import Shop from './components/react_router_tutorial/Shop';
+import axios from "axios";
+
+import Nav from "./components/react_router_tutorial/Nav";
+import About from "./components/react_router_tutorial/About";
+import Shop from "./components/react_router_tutorial/Shop";
 
 function App() {
   return (
@@ -16,15 +18,41 @@ function App() {
           <Route path="/about/" component={About} />
           <Route path="/shop/" component={Shop} />
         </Switch>
-      </div>    
+      </div>
     </Router>
   );
 }
 
-const Home = () => (
-  <div>
-    <h1>Home Page</h1>
-  </div>
-)
+class Home extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = { title : "no title" };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Home1 Page</h1> 
+        <div>
+          <button onClick={this.handleClick}>Click me</button>
+        </div>
+        <p>{this.state.title}</p>
+      </div>
+    )
+  }
+
+  handleClick = () => {
+    axios
+    .get("http://127.0.0.1:8000/api/entries", { params: {} })
+    .then((results) => {
+      this.setState({title: results.data[0].title})
+    })
+    .catch((err) => {
+      console.log("ERROR: ", err);
+    });
+  }
+}
 
 export default App;
